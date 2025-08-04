f590ea8add64445459b1777b05fc82ea65bf751b
Paul Chiu <paulchiu@gmail.com>
2025-08-04 18:45:45 +1000

    feat(github): Add CI and publish workflows with Dependabot configuration
    - feat(dependabot): Create Dependabot configuration to manage npm dependencies
    - feat(ci): Setup CI pipeline for testing across multiple Node.js versions
    - feat(publish): Implement workflow to publish packages to NPM on version changes
    - feat(release): Add manual release workflow for version bumping and triggering publish

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ const config = {
   input: "src/index.js",
   external: Object.keys(meta.dependencies || {}).filter(key => /^d3-/.test(key)),
   output: {
-    file: `dist/${meta.name}.js`,
+    file: `dist/d3-color-common-js.js`,
     name: "d3",
     format: "umd",
     indent: false,
@@ -22,7 +22,7 @@ export default [
     ...config,
     output: {
       ...config.output,
-      file: `dist/${meta.name}.min.js`
+      file: `dist/d3-color-common-js.min.js`
     },
     plugins: [
       ...config.plugins,
